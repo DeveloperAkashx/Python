a901acb389b137f99e8ae218a57e08cf568ae533
Akash Patil <159530461+DeveloperAkashx@users.noreply.github.com>
2024-07-25 01:19:43 -0700

    Operations On Array

---
+++ OperationsOnArray.py
@@ -0,0 +1,9 @@
+import numpy as np 
+arr1 = [1,2,3,4]
+arr2 = [5,6,7,8]
+a = np.array(arr1)
+b = np.array(arr2)
+print("Additon is",a + 2)
+print("Subtraction is",a - 2)
+print("Multiplication is",a * 2)
+print("Sum of Two array is",a + b)
