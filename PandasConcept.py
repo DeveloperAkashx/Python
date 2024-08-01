1261359c03d177bf9d0b1bcd06e08d6d43088b45
Akash Patil <159530461+DeveloperAkashx@users.noreply.github.com>
2024-08-01 01:13:36 -0700

    Pandas Concept Experiment No.1

---
+++ PandasConcept.py
@@ -0,0 +1,8 @@
+import pandas as pd     # To Print in Order form
+
+s = pd.Series([1,2,3,4,5])         # Series
+print(s)
+
+data = {'A':[1,2,3],'B':[4,5,6]}      # DataFrame
+df = pd.DataFrame(data)
+print(df)
