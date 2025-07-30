8144eabfae414f5d732cc681dd082f6ffa999e80
Akash Patil <159530461+DeveloperAkashx@users.noreply.github.com>
2025-07-30 10:19:04 -0700

    Day 3 Python Practice

--- Prac8.py
+++ Prac8.py
@@ -1,11 +1,15 @@
-list = [1,"abc","abc",1]
+def factorial(n):
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+    return result
 
-newlist = list.copy()
-newlist.reverse()
+def permutation(n, r):
+    if r > n:
+        return "Invalid: r cannot be greater than n."
+    return factorial(n) // factorial(n - r)
 
-print(newlist)
+n = int(input("Enter value of n: "))
+r = int(input("Enter value of r: "))
 
-if(list == newlist):
-    print("List is Palindrone")
-else:
-    print("List is not Palindrone")
+print(f"Permutation (nPr) is: {permutation(n, r)}")
