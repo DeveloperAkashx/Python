7837fd5fc4c9bfd5a5b6b976777a8b95c3514dca
Akash Patil <159530461+DeveloperAkashx@users.noreply.github.com>
2025-07-30 10:19:29 -0700

    Day 3 Python Practice

--- Prac9.py
+++ Prac9.py
@@ -1,7 +1,15 @@
+def factorial(n):
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+    return result
 
-list = []
-for i in range(1,11):
-    list.append(i*i)
-    
-print(list)
+def combination(n, r):
+    if r > n:
+        return "Invalid: r cannot be greater than n."
+    return factorial(n) // (factorial(r) * factorial(n - r))
 
+n = int(input("Enter value of n: "))
+r = int(input("Enter value of r: "))
+
+print(f"Combination (nCr) is: {combination(n, r)}")
