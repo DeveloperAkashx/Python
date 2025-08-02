032b46c56f247b6e0ce06a17306787f7eb1dd475
Akash Patil <159530461+DeveloperAkashx@users.noreply.github.com>
2025-08-02 11:39:05 -0700

    Day 3 Python Practice

--- Prac13.py
+++ Prac13.py
@@ -1,16 +1,2 @@
-text1 = "Python is a great programming language"
-text2 = "Many developers love the Python language"
-
-set1 = set(text1.split())
-set2 = set(text2.split())
-
-set3 = set1.intersection(set2)
-print(set3)
-
-set4 = set1.union(set2)
-print(set4)
-
-unique = set4.difference(set3)
-print(unique)
-
-print(len(unique))
+import os 
+os.remove("Prac9.py")
