c8c03320df79f3f983c31270d53fb18bc8635c0c
Akash Patil <159530461+DeveloperAkashx@users.noreply.github.com>
2025-08-02 11:39:22 -0700

    Day 3 Python Practice

--- Prac14.py
+++ Prac14.py
@@ -1,10 +1,14 @@
-dict = {
-    "name" : "Akash",
-    "marks" : {
-        "python" : 90,
-        "C" : 80,
-        "Java" : 99
-    }
-}
+myFile = open("Practice.txt","a")
 
-print(dict["marks"])
+myFile.write("\nHi everyone\nWe are learning File Handling I/O\nusing python\nI like programming in python")
+
+
+myFile = open("Practice.txt")
+
+data = myFile.read()
+newData = data.replace("python","JavaScript")
+myFile.close()
+
+myFile = open("Practice.txt","w")
+myFile.write(newData)
+myFile.close()
