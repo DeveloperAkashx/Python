3527d2ce2625e9498ac4df742157562fb6d45576
Akash Patil <159530461+DeveloperAkashx@users.noreply.github.com>
2025-08-03 12:33:46 -0700

    Day 4 Python Practice

--- Prac3.py
+++ Prac3.py
@@ -1,8 +1,21 @@
-list = [1,2,33,4,5,6,7]
+class Student:
+    college_name = "DKTE"
 
-def list_len(list):
-    return len(list)
-    
-print(list_len(list))
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def student_info(self):
+        print(f"Name = {self.name}")
+        print(f"Age = {self.age}")
+        print(f"College = {self.college_name}")
 
 
+s1 = Student("Akash",20)
+s1.student_info()
+
+s2 = Student("Viraj",18)
+s2.student_info()
+
+s3 = Student("Nik", 20)
+s3.student_info()
