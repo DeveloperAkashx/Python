1c469ca013ca5826b213ddc5811506e232b65da0
Akash Patil <159530461+DeveloperAkashx@users.noreply.github.com>
2025-08-03 12:34:13 -0700

    Day 4 Python Practice

--- Prac5.py
+++ Prac5.py
@@ -1,6 +1,24 @@
+class Student:
 
-def mysence(city="kolhapur"):
-    print("I'm From "+ city)
+    
+    def __init__(self, name , marks):
+        self.name = name
+        self.marks = marks
 
-mysence()
-mysence("Sangli") 
+    def student_info(self):
+        print(f"Name = {self.name}")
+        print(f"Marks = {self.marks}")
+
+    @staticmethod
+
+    def avg(self):
+        sum = 0
+        for i in self.marks:
+            sum += i
+            avg = sum/3
+            return avg
+
+s1 = Student("Akash",[90,80,70])
+ans = s1.avg()
+
+print(f"{s1.name}s avg marks is {ans}")
